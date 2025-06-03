0dae8ca25f4aebc05bc3be52f7344ab1e7215ca0
Abdul Matyn Talib <abdulmatyn@gmail.com>
2025-06-03 12:23:55 +0800

    add a label text and change background colour

--- Calculator_Again/Calculator_Again/Form1.Designer.cs
+++ Calculator_Again/Calculator_Again/Form1.Designer.cs
@@ -47,6 +47,7 @@
             this.divide = new System.Windows.Forms.Button();
             this.multiply = new System.Windows.Forms.Button();
             this.clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textdisplay
@@ -299,12 +300,23 @@
             this.clear.UseVisualStyleBackColor = false;
             this.clear.Click += new System.EventHandler(this.Clear_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(589, 358);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "testing";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.Maroon;
             this.ClientSize = new System.Drawing.Size(783, 476);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.clear);
             this.Controls.Add(this.add);
             this.Controls.Add(this.minus);
@@ -353,6 +365,7 @@
         private System.Windows.Forms.Button divide;
         private System.Windows.Forms.Button multiply;
         private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label1;
     }
 }
 
